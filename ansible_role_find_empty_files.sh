923456352ef1bf15dad79b6b0ed867f749a2ac11
Robert de Bock <robert@meinit.nl>
2021-01-05 15:42:40 +0100

    Simpler.

--- ansible_role_find_empty_files.sh
+++ ansible_role_find_empty_files.sh
@@ -1,22 +1,19 @@
 #!/bin/sh
 
-validate_environment() {
-  for binary in wc ; do
-    which "${binary}" > /dev/null 2>&1 || (echo "Missing ${binary}, please install it." ; exit 1)
-  done
-}
+for binary in wc ; do
+  which "${binary}" > /dev/null 2>&1 || (echo "Missing ${binary}, please install it." ; exit 1)
+done
 
 checker() {
   if [ -f "${1}/main.yml" ] ; then
     count=$(cat "${1}/main.yml" | wc -l)
-    if [ "${count}" -le 3 ] ; then
+    if [ "${count}" -le 2 ] ; then
       echo "The file ${1}/main.yml is empty."
-      exit 1
+      return 1
     fi
   fi
 }
 
-validate_environment
 for type in defaults handlers vars ; do
   checker ${type}
 done
