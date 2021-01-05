0201ebae9f09e7c2a5fe338e16168fa6ff7efb19
Robert de Bock <robert@meinit.nl>
2021-01-05 15:41:35 +0100

    Collect errors, fail when errors are found. Fixes #1.

--- ansible_role_find_unused_variable.sh
+++ ansible_role_find_unused_variable.sh
@@ -2,31 +2,28 @@
 
 # A script to find unused variables in Ansible roles.
 
-# You can list variables that are excluded from checks. This is a regular expressions used with `grep`.
-variable_whitelist="(service_list)"
-
-validate_environment() {
-  for binary in grep cut wc ; do
-    which "${binary}" > /dev/null 2>&1 || (echo "Missing ${binary}, please install it." ; exit 1)
-  done
-}
+for binary in grep cut wc ; do
+  which "${binary}" > /dev/null 2>&1 || (echo "Missing ${binary}, please install it." ; exit 1)
+done
 
 checker() {
   type="${1}"
   if [ -d ${type} -a -f ${type}/main.yml ] ; then
-    cat ${type}/main.yml | grep -v '^#' | grep -v '^$' | grep -v -- '---' | grep -v '^ ' | grep -v '^_' | grep -vE "${variable_whitelist}" | cut -d: -f1 | while read variable ; do
+    cat ${type}/main.yml | grep -v '^#' | grep -v '^$' | grep -v -- '---' | grep -v '^ ' | grep -v '^_' | cut -d: -f1 | while read variable ; do
       matches=$(grep -Rilw "${variable}" | grep -vE '(tasks/assert.yml|README.md)' | wc -l)
       internalmatches=$(grep -icw "${variable}" ${type}/main.yml)
       if [ ${matches} -le 1 -a ${internalmatches} -le 1 ] ; then
         echo "${type}/main.yml defines ${variable} which is not used."
-        exit 1
       fi
     done
   fi
 }
 
-validate_environment
+# Save the errors in a variable "errors".
+errors=$(for type in  defaults vars ; do checker "${type}" ; done)
 
-for type in  defaults vars ; do
-  checker "${type}"
-done
+# If the "errors" variable has content, something is wrong.
+if [ ! -z "${errors}" ] ; then
+  echo "${errors}"
+  exit 1
+fi
