460d098526a7705ebf914cd3a177e76e79b3d445
Robert de Bock <robert@meinit.nl>
2021-09-06 16:10:26 +0200

    make it work.

--- ansible_role_find_empty_files.sh
+++ ansible_role_find_empty_files.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/sh -x
 
 which wc > /dev/null 2>&1 || (echo "Missing wc, please install it." ; exit 1)
 
 checker() {
   if [ -f "${1}/main.yml" ] ; then
-    count=$(wc -l << "${1}/main.yml")
+    count=$(cat "${1}/main.yml" | wc -l)
     if [ "$(( count * 1 ))" -le 2 ] ; then
       echo "The file ${1}/main.yml is empty."
       return 1
