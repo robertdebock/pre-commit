bd34ccafb7eefbfc0891b62135305bf64cd7f7c3
Robert de Bock <robert@meinit.nl>
2021-09-06 16:16:12 +0200

    All errors gone?

--- ansible_role_find_empty_files.sh
+++ ansible_role_find_empty_files.sh
@@ -1,10 +1,10 @@
-#!/bin/sh -x
+#!/bin/sh
 
 which wc > /dev/null 2>&1 || (echo "Missing wc, please install it." ; exit 1)
 
 checker() {
   if [ -f "${1}/main.yml" ] ; then
-    count=$(cat "${1}/main.yml" | wc -l)
+    count=$(wc -l < "${1}/main.yml")
     if [ "$(( count * 1 ))" -le 2 ] ; then
       echo "The file ${1}/main.yml is empty."
       return 1
