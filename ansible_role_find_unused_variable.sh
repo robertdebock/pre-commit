0d338bd406ac37b7c1ebac599085104feb65be43
Robert de Bock <robert@meinit.nl>
2021-10-06 15:24:37 +0200

    A little wider to also find "{{ one + two }}".

--- ansible_role_find_unused_variable.sh
+++ ansible_role_find_unused_variable.sh
@@ -10,8 +10,8 @@ checker() {
   type="${1}"
   if [ -d "${type}" ] && [ -f "${type}/main.yml" ] ; then
     grep -v '^#' "${type}/main.yml" | grep -v '^$' | grep -v -- '---' | grep -v '^ ' | grep -v '^_' | cut -d: -f1 | while read -r variable ; do
-      matches="$(grep -Rilw "${variable}" -- * | grep -vEc '(tasks/assert.yml|README.md)')"
-      internalmatches="$(grep -icw "${variable}" "${type}/main.yml")"
+      matches="$(grep -Ril "${variable}" -- * | grep -vEc '(tasks/assert.yml|README.md)')"
+      internalmatches="$(grep -ic "${variable}" "${type}/main.yml")"
       if [ "${matches}" -le 1 ] && [  "${internalmatches}" -le 1 ] ; then
         echo "${type}/main.yml defines ${variable} which is not used."
       fi
