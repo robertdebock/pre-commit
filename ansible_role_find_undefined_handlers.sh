3c3f9cde783b2d96fa595f14b6fdadd4bcfa859d
Robert de Bock <robert@meinit.nl>
2021-10-07 16:53:20 +0200

    Fix test script, add undefined_handler checker.

---
+++ ansible_role_find_undefined_handlers.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# A script to find handlers that are called but not defined in Ansible roles.
+
+for binary in grep cut sort uniq awk sed ; do
+  which "${binary}" > /dev/null 2>&1 || (echo "Missing ${binary}, please install it." ; exit 1)
+done
+
+checker() {
+  # See if there any handlers.
+  if [ -d handlers ] && [ -f handlers/main.yml ] ; then
+    # See if there are any handlers called by notify.
+    for file in tasks/*.yml ; do
+      if grep -q 'notify:' "${file}" ; then
+        # Filter out the called handlers.
+        notification=$(awk '$1 == "-"{ if (key == "notify:") print $0; next } {key=$1}' "${file}"| sed 's/ *- //' | sort | uniq)
+        echo "${notification}" | while read notify ; do
+          if ! grep -q -- "- name: ${notify}" handlers/main.yml ; then
+            echo "The notification to \"${notify}\" in ${file} is not mention in handlers/main.yml."
+          fi
+        done
+      fi
+    done
+  fi
+}
+
+# Save the errors in a variable "errors".
+errors=$(checker)
+
+# If the "errors" variable has content, something is wrong.
+if [ -n "${errors}" ] ; then
+  echo "${errors}"
+  exit 1
+fi
+
--- tests/test.sh
+++ tests/test.sh
@@ -5,31 +5,39 @@ test_role() {
   expected_unused_variables="${2}"
   expected_empty_files="${3}"
   expected_empty_directories="${4}"
+  expected_undefined_handlers="${5}"
 
   cd "${role}" || return
     unused_variables=$(../../ansible_role_find_unused_variable.sh | wc -l)
     empty_files=$(../../ansible_role_find_empty_files.sh | wc -l)
     empty_directories=$(../../ansible_role_find_empty_directories.sh | wc -l)
+    undefined_handlers=$(../../ansible_role_find_undefined_handlers.sh | wc -l)
   cd ../
   
-  if [ "${unused_variables}" != "${expected_unused_variables}" ] ; then
+  if [ "$(( unused_variables * 1 ))" -ne "${expected_unused_variables}" ] ; then
     echo "${role} shows ${unused_variables} unused variables, expecting ${expected_unused_variables}."
     return 1
   fi
   
-  if [ "${empty_files}" != "${expected_empty_files}" ] ; then
+  if [ "$(( empty_files * 1))" -ne "${expected_empty_files}" ] ; then
     echo "${role} shows ${empty_files} empty files, expecting ${expected_empty_files}."
     return 1
   fi
 
-  if [ "${empty_directories}" != "${expected_empty_directories}" ] ; then
+  if [ "$(( empty_directories * 1 ))" -ne "${expected_empty_directories}" ] ; then
     echo "${role} shows ${empty_directories} empty directories, expecting ${expected_empty_directories}."
+    return 1
+  fi
+
+  if [ "$(( undefined_handlers * 1 ))" != "${expected_undefined_handlers}" ] ; then
+    echo "${role} shows ${undefined_handlers} undefined handlers, expecting ${expected_undefined_handlers}."
     return 1
   fi
 }
 
-# This runs the tests and tess the test_role function what to expect.
-test_role ansible-role-correct           0 0 0
-test_role ansible-role-unused-variables  3 0 0
-test_role ansible-role-empty_files       0 3 0
-test_role ansible-role-empty_directories 0 0 3
+# This runs the tests and tests the test_role function what to expect.
+test_role ansible-role-correct            0 0 0 0
+test_role ansible-role-unused-variables   3 0 0 0
+test_role ansible-role-empty_files        0 3 0 0
+test_role ansible-role-empty_directories  0 0 3 0
+test_role ansible-role-undefined_handlers 0 0 0 2
