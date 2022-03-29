e75909016a6ff5d04a81be6c94b049082eb50d52
Robert de Bock <robert@meinit.nl>
2022-03-29 15:35:58 +0200

    Update values.

--- ansible_role_find_unquoted_values.sh
+++ ansible_role_find_unquoted_values.sh
@@ -9,17 +9,20 @@ done
 checker() {
   directory="${1}"
   if [ -d "${directory}" ] ; then
-    matches=$(find "${directory}" -name '*.yml' -exec grep '^.*: [0-9]*\..*$' {} \; | wc -l)
+    version_pattern='[a-zA-z0-9\-\_]*: [0-9].*\.'
+    colon_pattern='[a-zA-z0-9\-\_]*: [a-zA-z0-9\-\_].*:.*'
+    pattern="(${version_pattern}|${colon_pattern})"
+    matches=$(find "${directory}" -name '*.yml' -exec grep -E "${pattern}" {} \; | wc -l)
     if [ -n "${matches}" ] ; then
       if [ "${matches}" -gt 0 ] ; then
-        echo "Found $((matches * 1)) unquoted values in ${directory}."
+        echo "Found $((matches * 1)) risky and unquoted values in ${directory}."
       fi
     fi
   fi
 }
     
 # Save the errors in a variable "errors".
-errors=$(for directory in defaults tasks meta molecule vars ; do checker "${directory}" ; done)
+errors=$(for directory in defaults handlers tasks meta molecule vars ; do checker "${directory}" ; done)
 
 # If the "errors" variable has content, something is wrong.
 if [ -n "${errors}" ] ; then
